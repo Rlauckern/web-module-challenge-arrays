1b4cc94bdf0cd413a663ceb0de02990df9706915
Carlton Lauckern <reed@Carltons-MacBook-Air.local>
2021-11-03 10:27:19 -0700

    question-2

--- index.js
+++ index.js
@@ -44,10 +44,10 @@ Use the copy function below to do the following:
   1. receive an array as a parameter - you will pass in originalFlavors as an argument when the function is invoked.
   2. Return a copy of the received array  
 */
-
-function copy(/*your code here*/){
-  /*your code here*/
+function copy(array){
+  return [...array];
 }    
+console.log('task 1', copy(originalFlavors));
 
 
 
@@ -64,10 +64,16 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
- /*your code here*/
-}
-
+function is31Flavors(array){
+  if(array.length === 31){
+    return true;
+  }else{
+    return false;
+  }
+ }
+ 
+ console.log('task 2:', is31Flavors(originalFlavors));
+ 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
 
